e88530150e282f122a6298077a9565b6b934c7b6
Andy Ball <andy_ball@32097.gnm.int>
2017-08-03 13:12:34 +1000

    Update to allow console log preview in apps via a console panel.

--- src/js/embed.js
+++ src/js/embed.js
@@ -4,6 +4,22 @@ import embedHTML from './text/embed.html!text'
 
 window.init = function init(el, config) {
 
-    console.log("Testing page load")
+    // Good for testing output of console log on apps.
+
+    var logs = document.getElementById('console_panel');
+
+    console.log = function(message) {
+
+        logs.innerHTML += '<p>' + message + '</p>';
+
+    };
+
+    console.error = console.debug = console.info =  console.log
+
+    console.log("Test insertion of CSS")
+
+	$( document ).ready(function() {
+	    console.log($('body').html());
+	});
 
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,30 @@
-export function init() {
-	console.log("Test insertion of CSS")
+import mainHTML from './text/main.html!text'
+import $ from './lib/jquery'
+
+export function init(el, context, config, mediator) {
+
+	/*
+
+	el.innerHTML = mainHTML.replace(/%assetPath%/g, config.assetPath);
+
+    // Good for testing output of console log on apps.
+
+    var logs = document.getElementById('console_panel');
+
+    console.log = function(message) {
+
+        //logs.innerHTML += '<p>' + message + '</p>';
+
+        $('#console_panel').text(message);
+
+    };
+
+    console.error = console.debug = console.info =  console.log
+    
+	$( document ).ready(function() {
+	    console.log($('body').html());
+	});
+
+	*/
+
 }
