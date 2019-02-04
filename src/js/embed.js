1b99dd4561ddb4cac7a5c393289d8b52d9c0c68a
Andy Ball <work@andyball.info>
2019-02-04 12:11:02 +1100

    Updated styles for book page and removed old files

--- src/js/embed.js
+++ src/js/embed.js
@@ -1,25 +1,7 @@
 import iframeMessenger from 'guardian/iframe-messenger'
-import reqwest from 'reqwest'
 import embedHTML from './text/embed.html!text'
 
 window.init = function init(el, config) {
 
-    // Good for testing output of console log on apps.
-
-    var logs = document.getElementById('console_panel');
-
-    console.log = function(message) {
-
-        logs.innerHTML += '<p>' + message + '</p>';
-
-    };
-
-    console.error = console.debug = console.info =  console.log
-
-    console.log("Test insertion of CSS")
-
-	$( document ).ready(function() {
-	    console.log($('body').html());
-	});
 
 };
